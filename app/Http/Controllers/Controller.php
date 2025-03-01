7b47e572b175ca94a67b8d46f8d28081fb8499c1
sagbo Francis <francissagbo1@gmail.com>
2025-03-01 16:52:47 +0100

    Mise à jour

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produit;
+use App\Models\User;
+use Illuminate\Http\Request;
+
 abstract class Controller
 {
     //
+    public function index(){
+        return response()->json(User::all());
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nom'=>'required|string',
+            'description'=>'required|string',
+            
+            'prix'=>'required|decimal',
+            'quantité'=>'required|integer'
+        ]);
+        $produit=Produit::create([
+            'nom'=>$request->nom,
+            'description'=>$request->description,
+            'prix'=>$request->prix,
+            'quantité'=>$request->quantité,
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+        
+        return response()->json($produit, 201);   
+    }
+    public function show($id){
+        $produit=Produit::find($id);
+         
+    }
 }
